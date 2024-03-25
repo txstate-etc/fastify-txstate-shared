377fd84712fbbb89da0d33cc0deb35eb837649e4
Nick Wing <nwing@txstate.edu>
2024-03-25 16:56:20 -0500

    yet another esm fix, release 1.0.9

--- lib-esm/index.js
+++ lib-esm/index.js
@@ -1,4 +1,4 @@
-import mod from '../lib'
+import mod from '../lib/index.js'
 export const interactionEvent = mod.interactionEvent
 export const validatedResponse = mod.validatedResponse
 export const validationMessage = mod.validationMessage
