6226c6db8fab79c5bdd21834ed90d623d6b014aa
Nick Wing <nwing@txstate.edu>
2024-03-25 15:55:59 -0500

    export validationMessage and validatedResponse in ESM, release 1.0.6

--- lib-esm/index.js
+++ lib-esm/index.js
@@ -1,2 +1,4 @@
 const mod = require('../lib')
 export const interactionEvent = mod.interactionEvent
+export const validatedResponse = mod.validatedResponse
+export const validationMessage = mod.validationMessage
